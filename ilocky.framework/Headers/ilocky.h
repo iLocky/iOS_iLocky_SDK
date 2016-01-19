9a9dbfc6cb3d2acc998d124dbf53a70e721c7dbf
Richard Chin <richjing@gmail.com>
2016-01-19 20:43:58 +0800

    v1.1 bug fixed

---
+++ ilocky.framework/Headers/ilocky.h
@@ -0,0 +1,19 @@
+//
+//  ILocky.h
+//  ILocky
+//
+//  Created by Richard on 2015/9/21.
+//  Copyright © 2015年 Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "mixpanel.h"
+//! Project version number for ILocky.
+FOUNDATION_EXPORT double ILockyVersionNumber;
+
+//! Project version string for ILocky.
+FOUNDATION_EXPORT const unsigned char ILockyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ILocky/PublicHeader.h>
+
+
